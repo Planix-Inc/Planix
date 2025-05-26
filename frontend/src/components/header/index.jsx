b30f19e132f4a383a28e25039619c7e98ce7dcac
Android Studio <devandroid@ort>
2025-05-26 09:12:28 -0300

    correccion

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,6 +1,5 @@
 import './header.css';
 import logo from '../../assets/Logos/logo.png';
-import { Link } from "react-router-dom";
 
 function Header() {
     return (
@@ -10,10 +9,10 @@ function Header() {
                     <img src={logo} alt="Logo Planix" />
                 </div>
                 <nav className="navegacion">
-                    <Link to="./pages/profesionales/index.jsx">Profesionales</Link>
-                    <Link to="">Proveedores</Link>
-                    <Link to="">Proyectos</Link>
-                    <Link to="">Constructoras</Link>
+                    <a href="#">Profesionales</a>
+                    <a href="#">Profesionales</a>
+                    <a href="#">Profesionales</a>
+                    <a href="#">Profesionales</a>
                 </nav>
                 <div className="botones">
                     <button className="btn-iniciar">Iniciar sesión</button>
@@ -23,5 +22,4 @@ function Header() {
         </header>
     );
 }
-
 export default Header;

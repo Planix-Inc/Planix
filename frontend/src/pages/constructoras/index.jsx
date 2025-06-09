bcbaa64819a973285294b218c134f43b80b2152c
Android Studio <devandroid@ort>
2025-06-09 11:55:26 -0300

    Avance

--- frontend/src/pages/constructoras/index.jsx
+++ frontend/src/pages/constructoras/index.jsx
@@ -7,6 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 const Constructoras = () => {
   const [constructoras, setConstructoras] = useState([]);
+  const [constructorasDestacado, setConstructorasDestacado] = useState([]);
 
   useEffect(() => {
     const fetchConstructoras = async () => {
@@ -22,7 +23,22 @@ const Constructoras = () => {
       }
     };
 
+    const fetchConstructorasDestacados = async () => {
+      const { data: Usuario, error } = await supabase
+        .from("Usuario")
+        .select("*")
+        .eq("categoriausuarioId", 3)
+        .eq("destacado", true);
+
+      if (error) {
+        console.error("Error al obtener constructoras:", error);
+      } else {
+        setConstructorasDestacado(Usuario);
+      }
+    };
+
     fetchConstructoras();
+    fetchConstructorasDestacados();
   }, []);
 
   const configuracionCarrusel = {
@@ -67,12 +83,28 @@ const Constructoras = () => {
           type="text" 
           placeholder="Buscar constructoras para tus proyectos" 
         />
-        <button className="filter-button">🔍</button>
+        <button className="filter-button"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 50 50"><path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path></svg></button>
       </div>
     </div>
   </div>
 
   <div className="seccion-constructoras">
+  <h2 className="titulo-seccion">Constructoras Destacados</h2>
+        <Slider {...configuracionCarrusel} className="carrusel-constructoras">
+          {constructorasDestacado.map((prof) => (
+            <div key={prof.id} className="tarjeta-constructoras">
+              <div className="imagen-constructoras">
+                <img src={prof.img} alt={`${prof.nombre} ${prof.apellido}`} />
+              </div>
+              <h3 className="nombre-constructoras">
+                {prof.nombre} {prof.apellido}
+              </h3>
+              <p className="texto-localidad">📍 {prof.localidad} - ⭐ {prof.valoracion}</p>
+              <button className="boton-ver-perfil">Ver perfil</button>
+            </div>
+          ))}
+        </Slider>
+
         <h2 className="titulo-seccion">Constructoras</h2>
         <Slider {...configuracionCarrusel} className="carrusel-constructoras">
           {constructoras.map((prof) => (
@@ -83,7 +115,7 @@ const Constructoras = () => {
               <h3 className="nombre-constructoras">
                 {prof.nombre} {prof.apellido}
               </h3>
-              <p className="texto-localidad">{prof.localidad}</p>
+              <p className="texto-localidad">📍 {prof.localidad} - ⭐ {prof.valoracion}</p>
               <button className="boton-ver-perfil">Ver perfil</button>
             </div>
           ))}
